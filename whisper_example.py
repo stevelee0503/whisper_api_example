8e0a0d4011d5814b652791a895b69568b55aac42
HAE YONG LEE <125369859+stevelee0503@users.noreply.github.com>
2023-03-02 17:14:43 +0900

    Init commit
    Write some sample code to use open AI Whisper API

---
+++ whisper_example.py
@@ -0,0 +1,16 @@
+import openai
+import os
+
+# First, set your OpenAI API key as an environment variable
+os.environ["OPENAI_API_KEY"] = "Enter your API key here"
+
+# Create the OpenAI API client
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# Set the model you want to use
+model_engine = "gpt-3.5-turbo"
+file = open("파이썬강의오디오.mp3", "rb")
+
+transcription = openai.Audio.transcribe("whisper-1", file)
+
+print(transcription['text'])
