aedf8826e7b802f563ae2cbf2c64f447ebe26759
HAE YONG LEE <125369859+stevelee0503@users.noreply.github.com>
2023-03-02 20:00:22 +0900

    Change audio file name

--- whisper_example.py
+++ whisper_example.py
@@ -9,7 +9,7 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Set the model you want to use
 model_engine = "gpt-3.5-turbo"
-file = open("파이썬강의오디오.mp3", "rb")
+file = open("파이썬강의.mp3", "rb")
 
 transcription = openai.Audio.transcribe("whisper-1", file)
 
